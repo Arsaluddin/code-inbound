aa7be923ad8f74caec8f3ca7384b46fc887a5e32
arsal uddin <arsaluddin134@gmail.com>
2023-08-31 12:54:52 +0530

    connected

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config()
+const { Client } = require('pg')
+
+
+// const userRoutes = require('./routes/userRoutes');
+const PORT = process.env.PORT || 3000;
 
 // Middleware for parsing JSON
 app.use(express.json());
 
-// Define your routes and endpoints here
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'code-inbound',
+    password: process.env.PASSWORD,
+    port: 5432,
+  })
+  client.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
